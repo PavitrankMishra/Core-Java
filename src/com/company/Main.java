adb5fe2fda456c8fe342dae7d4dab3a383015ff0
Pavitrank <pavitrank2002@gmail.com>
2024-01-20 22:17:07 +0530

    OOPS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,36 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to core java");
+//        Store 5 roll nos
+        int[] numbers = new int[5];
+
+//        Store 5 names
+        String[] names = new String[5];
+
+//        data of 5 students: (roll no, name, marks)
+        int[] rno = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+
+        Student[] students = new Student[5];
+
+        Student stud1;//Just declaring the object of Student class
+//        System.out.println(Arrays.toString(students));
+//        student1 = new Student();
+//        New keyword dynamically allocates memory and return a reference to it
+
+
+//        Create a class
+//        Class - Prototype of an object
+//        The rules which help to develop object
+//        Prototype - Instance of a class
+//        Actually visible which uses some rules to be created
+//        Instance variable - Variable Inside the o
+
     }
+}
+class Student {
+    int rno;
+    String name;
+    float marks;
 }
