b490336924ef7942011b91cd3aaef8e69f4a716a
Pavitrank <pavitrank2002@gmail.com>
2024-03-29 10:22:57 +0530

    New Commit

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -11,15 +11,13 @@ public class BinarySearch {
     static int binarySearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length-1;
-
         while(start<=end) {
-            //Find the middle element
-//            int mid = (start + end)/2;//Might be posibble that (start + end) goes out of the range of int.
-            int  mid = start + (end - start) / 2;
-
-            if(target < arr[mid]) {
-                end = mid-1;
-            } else if(target > arr[mid]) {
+//            Find the mid element
+//            int mid = (start+end)/2;//Might be possible that (start+end) goes out of the range of int.
+            int mid = start + (end - start) / 2;
+            if (target < mid) {
+                end = mid - 1;
+            } else if (target > mid) {
                 start = mid + 1;
             } else {
                 return mid;
--- src/com/company/Hashing.java
+++ src/com/company/Hashing.java
@@ -1,75 +1,99 @@
 package com.company;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.*;
+
 public class Hashing {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-//        Create a hash table
-        Hashtable<Integer, String> hm = new Hashtable<Integer,String>();
-//        hm.put(3,"You are visiting");
-//        hm.put(5,"Hello");
-//        hm.put(1,"Website");
-//        hm.put(2,"Javatpoint");
-//        System.out.println(hm);
+        HashSet<Integer> set = new HashSet<Integer>();
+//      ArrayList<Integer> al = new ArrayList<Integer>();
 
-//        Creates an empty hashmap
-//        HashMap<Integer,Integer> hm1 = new HashMap<Integer,Integer>();
-////        System.out.println(hm1);
-//        int[] arr = {1,6,5,10,6,6,10};
-//
-//        for(int i=0;i<arr.length;i++) {
-//            Integer n = hm1.get(arr[i]);
-//
-//            if(hm1.get(arr[i]) == null) {
-//                hm1.put(arr[i], 1);
-//            } else {
-//                hm1.put(arr[i], ++n);
-//            }
-//        }
-//
-//        System.out.println(hm1);
+//      Insert
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
 
-//        Hashmap striver
-//        Taking input for array
-        System.out.println("Enter the length of the array");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++) {
-            arr[i] = sc.nextInt();
+        System.out.println("The size of the set is: " + set.size());
+//      Print all elements
+        System.out.println(set);
+
+//      SEARCH - CONTAINS
+        if(set.contains(1)) {
+            System.out.println("Set contains 1");
+        }
+        if(!set.contains(6)) {
+            System.out.println("Set does not contain 6");
         }
 
-//        Precompute
-        int[] hash = new int[13];
-        for(int i=0;i<n;i++) {
-            hash[arr[i]] += 1;
+        //Delete
+        set.remove(1);
+        if(!set.contains(1)) {
+            System.out.println("Does not contain 1");
         }
 
-        int q;
-        q = sc.nextInt();
-        while(q-- > 0) {
-            int number;
-            number = sc.nextInt();
-            System.out.println(hash[number]);
+        System.out.println("The size of set is: " + set.size());
+
+//        Print all elements
+        System.out.println(set);
+
+//        Iterator
+        Iterator it = set.iterator();
+//        hasNext();
+//        next();
+
+        while(it.hasNext()) {
+            System.out.println(it.next());
         }
+
+//        country(key), population(value)
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+//        Insertion
+        map.put("India", 120);
+        map.put("US", 30);
+        map.put("China", 150);
+
+        System.out.println(map);
+
+        map.put("China", 180);
+        System.out.println(map);
+
+
+//        Search
+        if(map.containsKey("China")) {
+            System.out.println("Key is present in the map");
+        } else {
+            System.out.println("Key is not present in the map");
         }
+
+        System.out.println(map.get("China"));//Key exists
+        System.out.println(map.get("Indonesia"));//Key does not exist
+
+        int[] arr = {12,15,18};
+        for(int i=0;i<3;i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+
+        for(int val:arr) {
+            System.out.println(val + " ");
+        }
+        System.out.println();
+
+        for(Map.Entry<String, Integer> e : map.entrySet()) {
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
+
+        Set<String> keys = map.keySet();
+        for(String key : keys) {
+            System.out.println(keys + " " + map.get(key));
+        }
+
+        map.remove("China");
+        System.out.println(map);
     }
-//        int n = 5;
-//        int[] arr = new int[n];
-////        Take the input of array
-//        for(int i=0;i<n;i++) {
-//            System.out.println("Enter the element: ");
-//            arr[i] = sc.nextInt();
-//        }
-//
-//        int[] hash = new int[13];
-//        for(int i=0;i<n;i++) {
-//            hash[arr[i]] += 1;
-//        }
-//
-//        int q;
-//        q = sc.nextInt();
-//        while(q-- > 0) {
-//            int number;
-//            number = sc.nextInt();
-//            System.out.println(hash[number]);
-//}
+}
+
